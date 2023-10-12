f6142396c84b9b35ea3a785de273e47fc6add9b5
Lancedamalerio <144929231+Lancedamalerio@users.noreply.github.com>
2023-10-12 18:45:21 +0800

    Update settings.py, fixed allowed hosts

--- midterm_aguandhischipmunks/widget_aguandhischipmunks/widget_aguandhischipmunks/settings.py
+++ midterm_aguandhischipmunks/widget_aguandhischipmunks/widget_aguandhischipmunks/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-z@+iegqn)^k4^=%rnr-$3*2+gy&7s1=%m$&%h!w_k%m*h+nl=&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ejrgus1755.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
